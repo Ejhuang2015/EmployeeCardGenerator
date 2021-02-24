bc04f99a32eaed73be379ea0f09eff64dfeab89f
Edward Huang <ejhuang.2015@gmail.com>
2021-02-24 18:27:01 -0500

    Added Manager card generation function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const Employee = require("./lib/Employee"); 
+const Manager = require("./lib/Manager"); 
 const { finished } = require("stream");
 
 
